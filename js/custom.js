297b853ce1316c0bdfbbcf9944bc62226ea873d5
Mathieu Ruellan <mathieu.ruellan@gmail.com>
2016-09-19 20:10:07 +0200

    Menu improvement

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,11 @@
-// Closes the sidebar menu
-$("#menu-close").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-
-// Opens the sidebar menu
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active");
-});
-
 // Scrolls to the selected menu item on the page
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        $('html,body').animate({
+        $('html, body').animate({
           scrollTop: target.offset().top
         }, 1000);
         return false;
@@ -29,6 +16,11 @@ $(function() {
 
 // Map scrolling behaviour
 $(document).ready(function() {
+
+  $('.nav a').click(function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
   $('#map_iframe').addClass('scrolloff');
   $('#map').on('click', function () {
     $('#map_iframe').removeClass('scrolloff');
